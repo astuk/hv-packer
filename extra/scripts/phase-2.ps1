94f1a8546538ab890275349dc8f34407f42bdb0a
astuk <37668568+astuk@users.noreply.github.com>
2018-10-13 11:09:57 +0100

    Update Phase-2.ps1
    Change Timezone to GMT

--- extra/scripts/phase-2.ps1
+++ extra/scripts/phase-2.ps1
@@ -1,6 +1,6 @@
 # Phase 2 - generic and essential stuff
 Write-Output "Start of Phase-2"
-  Set-TimeZone -Id "Central European Standard Time" -Verbose
+  Set-TimeZone -Id "GMT Standard Time" -Verbose
   choco feature enable -n allowEmptyChecksums
   choco install conemu sysinternals dotnet4.7.2 -y --force
 Write-Output "End of Phase-2"
